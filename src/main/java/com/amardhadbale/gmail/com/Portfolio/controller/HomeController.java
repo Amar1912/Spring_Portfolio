2ece57b8ed6e5f1836d75e7701746b474fed3104
Amar Dhadbale <amardhadbale@gmail.com>
2026-06-27 13:06:14 +0530

    first commit

---
+++ src/main/java/com/amardhadbale/gmail/com/Portfolio/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.amardhadbale.gmail.com.Portfolio.controller;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+}
---
+++ src/test/java/com/amardhadbale/gmail/com/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amardhadbale.gmail.com.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
