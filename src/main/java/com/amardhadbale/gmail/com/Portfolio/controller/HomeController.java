ed6f2bc32901da9b72b3fc3e824f7e8c93d1a7c1
Amar Dhadbale <amardhadbale@gmail.com>
2026-06-27 23:35:55 +0530

    Home page created with HTML & CSS

--- src/main/java/com/amardhadbale/gmail/com/Portfolio/controller/HomeController.java
+++ src/main/java/com/amardhadbale/gmail/com/Portfolio/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.amardhadbale.gmail.com.Portfolio.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping({" ","/","/home"})
+    public String home(){
+        return "home";
+    }
 
 }
