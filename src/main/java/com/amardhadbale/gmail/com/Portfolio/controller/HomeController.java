38e8fad38e849b04c4499ac77d41f8c459d49ac3
Amar Dhadbale <amardhadbale@gmail.com>
2026-06-28 22:56:04 +0530

    Form Controller setup

--- src/main/java/com/amardhadbale/gmail/com/Portfolio/controller/HomeController.java
+++ src/main/java/com/amardhadbale/gmail/com/Portfolio/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.amardhadbale.gmail.com.Portfolio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -8,6 +10,22 @@ public class HomeController {
     @GetMapping({" ","/","/home"})
     public String home(){
         return "home";
+    }
+
+    @GetMapping({"/about"})
+    public String about(){
+        return "about";
+    }
+    @PostMapping("/contact")
+    public String receiveContact(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String subject,
+            @RequestParam String message
+    ){
+       System.out.println("Contact "+name+" "+email+" "+subject+" "+message);
+       return "home";
+
     }
 
 }
